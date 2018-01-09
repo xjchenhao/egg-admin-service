481ed8b509431c2907432450f86862a1cb2c8d74
xjchenhao <wy.chenhao@qq.com>
2018-01-09 10:33:10 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1515465031562_6723';
+
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
