afa906c2bc4d7f1723bca5237e0a5a3288747c5e
xjchenhao <wy.chenhao@qq.com>
2018-03-05 17:57:31 +0800

    logo指向首页

--- client/src/layouts/BasicLayout.js
+++ client/src/layouts/BasicLayout.js
@@ -109,7 +109,7 @@ class BasicLayout extends React.PureComponent {
       urlParams.searchParams.delete('redirect');
       window.history.replaceState(null, 'redirect', urlParams.href);
     } else {
-      return '/';
+      return '/home';
     }
     return redirect;
   }
