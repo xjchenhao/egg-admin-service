6fb3c4b9bb3320dae1d0950675c3703423b3f9ad
xjchenhao <wy.chenhao@qq.com>
2018-04-10 14:18:40 +0800

    增加initdb脚本

--- server/config/config.local.js
+++ server/config/config.local.js
@@ -20,7 +20,7 @@ module.exports = {
                 // 密码
                 password: '123123aa',
                 // 数据库名
-                database: 'admin_demo',
+                database: 'eas_basis',
             },
         },
         // 是否加载到 app 上，默认开启
