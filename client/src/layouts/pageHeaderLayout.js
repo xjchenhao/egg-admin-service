edbca8fa6da64ee8cbd5bda6026efb9728f84160
xjchenhao <wy.chenhao@qq.com>
2018-06-26 15:03:58 +0800

    修改浏览器tabs上的title

--- client/src/layouts/pageHeaderLayout.js
+++ client/src/layouts/pageHeaderLayout.js
@@ -4,6 +4,14 @@ import PageHeader from 'ant-design-pro/lib/PageHeader';
 import styles from './pageHeaderLayout.less';
 
 export default ({ children, wrapperClassName, top, ...restProps }) => {
+
+  // 修改浏览器tabs上的title
+  if(restProps.title){
+    document.title = restProps.title; 
+  }
+
+  // const breadcrumb = this.conversionBreadcrumbList();
+
   return (
   <div style={{ margin: '-24px -24px 0' }} className={wrapperClassName}>
     {top}
