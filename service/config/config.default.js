7c4e61193f87f29ce363bf83bc4e00296ff590a0
xjchenhao <wy.chenhao@qq.com>
2018-11-05 15:33:45 +0800

    如果使用alinode，config中这个参数是必填的.. 本来想就测试环境配置一下，看来不行

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -38,5 +38,17 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1515465031562_6723';
 
+  config.alinode = {
+    appid: '75828',
+    secret: '34b2b1673c38f2625bb42f456b5b8cf6d4db01b3',
+    error_log: [
+      path.join('/usr/local/logs', 'common-error.log'),
+      path.join('/usr/local/logs', 'stderr.log'),
+    ],
+    packages: [
+      path.join(appInfo.baseDir, 'package.json'),
+    ],
+  };
+
   return config;
 };
