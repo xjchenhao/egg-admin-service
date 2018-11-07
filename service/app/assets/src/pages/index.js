11e7983248cea89244435aec3a7012315afad35d
xjchenhao <wy.chenhao@qq.com>
2018-11-07 15:59:55 +0800

    消灭client

--- service/app/assets/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-// import Link from 'umi/link';
-import DocumentTitle from 'react-document-title';
-import {title} from './../utils/config';
-
-export default () =>
-  <DocumentTitle title={title}>
-    <div>欢迎访问后台管理系统</div>
-  </DocumentTitle>
--- service/app/assets/src/services/sys/main.js
+++
@@ -1,6 +0,0 @@
-import { stringify } from 'qs';
-import request from '../../utils/request';
-
-export async function sidebar(params) {
-  return request(`/nodeApi/sys/sidebar?${stringify(params)}`);
-}
